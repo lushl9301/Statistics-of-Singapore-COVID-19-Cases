257e099bb749c64ba7ad7b968cc5b469c82e97c1
lushl9301 <lushl9301@gmail.com>
2020-04-08 23:40:53 +0800

    <add> refactor useful functions to common.py

---
+++ common.py
@@ -0,0 +1,71 @@
+import re
+
+def remove_age(s):
+    return re.sub("\d+ year-old", "", s)
+
+
+def remove_date(s):
+    mth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    for m in mth:
+        element = "\d+ to \d+ " + m + " 2020"
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+        element = "\d+ to \d+ " + m + " 2019"
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+        element = "\d+ to \d+ " + m
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+        element = "\d+ to \d+ " + m
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+
+    for m in mth:
+        element = "\d+ " + m + " 2020"
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+        element = "\d+ " + m + " 2019"
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+
+    mth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    for m in mth:
+        element = "\d+ " + m
+        t = re.sub(element, "", s)
+        if t:
+            s = t
+    return s
+
+def remove_time(s):
+    mth = ["pm", "am"]
+    for m in mth:
+        element = "\d+" + m
+        t = re.sub(element, "", s, flags=re.I)
+        if t:
+            s = t
+        element = "\d+" + m
+        t = re.sub(element, "", s, flags=re.I)
+        if t:
+            s = t
+    return s
+
+def remove_covid(s):
+    t = re.sub("2019-nCov", "", s, flags=re.I)
+    if t:
+        s = t
+    t = re.sub("2019 novel", "", s, flags=re.I)
+    if t:
+        s = t
+    t = re.sub("COVID-19", "", s, flags=re.I)
+    if t:
+        s = t
+    t = re.sub("COVID 19", "", s, flags=re.I)
+    if t:
+        s = t
+    return s
--- generate_full_graph.py
+++ generate_full_graph.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import sys
-import re
+from common import *
 
 G = nx.Graph()
 
@@ -15,46 +15,6 @@ def addEdge(a, b):
             if i != j:
                 G.add_edge(i, j)
 
-def remove_age(s):
-    return re.sub("\d+ year-old", "", s)
-
-
-def remove_date(s):
-    mth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    for m in mth:
-        element = "\d+ " + m + " 2020"
-        t = re.sub(element, " " , s)
-        if t:
-            s = t
-    # s = re.sub("\d+ January 2020", "", s)
-    # s = re.sub("\d+ Jan", "", s)
-    # s = re.sub("\d+ February 2020", "", s)
-    # s = re.sub("\d+ Feb", "", s)
-    # s = re.sub("\d+ March 2020", "", s)
-    # s = re.sub("\d+ Mar", "", s)
-    # s = re.sub("\d+ April 2020", "", s)
-    # s = re.sub("\d+ Apr", "", s)
-    mth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    for m in mth:
-        element = "\d+ " + m
-        t = re.sub(element, " " , s)
-        if t:
-            s = t
-    return s
-
-def remove_time(s):
-    mth = ["pm", "am"]
-    for m in mth:
-        element = "\d+" + m
-        t = re.sub(element, " " , s, re.I)
-        if t:
-            s = t
-        element = "\d+" + m
-        t = re.sub(element, " " , s, re.I)
-        if t:
-            s = t
-    return s
-
 
 def analyze_line(s):
     """
@@ -67,13 +27,16 @@ def analyze_line(s):
     s = remove_age(s)
     s = remove_date(s)
     s = remove_time(s)
+    s = remove_covid(s)
 
     print(s)
 
     ### case 1 & 2
     x = re.search("[Cc]ase.+linked to.+[cC]ase.+", s)
     if x:
-        print("111111111")
+        t = re.search("year-old", s)
+        print("111111")
+
 
 def process_file(filename):
     f = open(filename, "r")
